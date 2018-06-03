7131e61a12b7eeba18659892e4b3617dc41f5826
MasterTyping <jumbak1234@gmail.com>
2018-06-04 00:24:32 +0900

    ManyCube

--- d3dpractice07/Shader.cpp
+++ d3dpractice07/Shader.cpp
@@ -35,16 +35,23 @@ D3D12_RASTERIZER_DESC CShader::CreateRasterizerState()
 
 	D3D12_RASTERIZER_DESC d3dRasterizerDesc;
 	::ZeroMemory(&d3dRasterizerDesc, sizeof(D3D12_RASTERIZER_DESC));
+	//와이어 프레임,솔리드선택 
 	d3dRasterizerDesc.FillMode = D3D12_FILL_MODE_WIREFRAME;
+	//뒷면 자르기 앞면 자르기 자르지 않기
 	d3dRasterizerDesc.CullMode = D3D12_CULL_MODE_BACK;
+	//앞면이 반시계방향인지 아닌지
 	d3dRasterizerDesc.FrontCounterClockwise = FALSE;
 	d3dRasterizerDesc.DepthBias = 0;
 	d3dRasterizerDesc.DepthBiasClamp = 0.0f;
 	d3dRasterizerDesc.SlopeScaledDepthBias = 0.0f;
+	//깊이 자르기 
 	d3dRasterizerDesc.DepthClipEnable = TRUE;
+	//멀티 샘플링 할건지 안할건지
 	d3dRasterizerDesc.MultisampleEnable = FALSE;
+	//안티 앨리어싱 할건지 안할건지
 	d3dRasterizerDesc.AntialiasedLineEnable = FALSE;
 	d3dRasterizerDesc.ForcedSampleCount = 0;
+
 	d3dRasterizerDesc.ConservativeRaster = D3D12_CONSERVATIVE_RASTERIZATION_MODE_OFF;
 	return(d3dRasterizerDesc);
 }
